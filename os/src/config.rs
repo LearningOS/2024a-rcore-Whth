b4afa678926136a79d0670a8f8a5f264d005c9ba
Whth <88489697+Whth@users.noreply.github.com>
2024-11-01 23:15:43 +0800

    chore(cfg): add

--- os/src/config.rs
+++ os/src/config.rs
@@ -23,3 +23,13 @@ pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - PAGE_SIZE;
 pub const CLOCK_FREQ: usize = 12500000;
 /// the physical memory end
 pub const MEMORY_END: usize = 0x88000000;
+
+/// the max number of tasks
+pub const BIG_STRIDE: u64 = 255;
+
+/// initial stride
+pub const INIT_STRIDE: u64 = 1;
+/// default priority
+pub const DEFAULT_PRIORITY: isize = 16;
+
+
